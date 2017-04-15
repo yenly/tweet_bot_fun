71b4250f131fb92ec5ad49befcdefdbe2775ab57
Yenly Ma <yencodes@gmail.com>
2017-04-14 20:09:46 -0700

    Install Watson api, pass tweets to tone analyzer and personality trait api for json feedback.

--- search_tweets.js
+++ search_tweets.js
@@ -1,5 +1,12 @@
 var Twit = require('twit');
 var config = require('./config');
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/V3');
+var PersonalityInsighsV3 = require('watson-developer-cloud/personality-insights/v3');
+var TA_config = require('./watson_config');
+var PT_config = require('./personality_config');
+
+var tone_analyzer = new ToneAnalyzerV3(TA_config);
+var personality_insights = new PersonalityInsighsV3(PT_config);
 
 var T = new Twit(config);
 
@@ -10,9 +17,10 @@ var params = {
 
 var user_params = {
   screen_name: '@mindfullycrafts',
-  count: 10,
+  count: 100,
   include_rts: false
 }
+
 
 // T.get('search/tweets', params, gotData);
 T.get('statuses/user_timeline', user_params, gotUserDatata);
@@ -28,7 +36,30 @@ function gotData(err, data, response) {
 function gotUserDatata(err, data, response) {
   var tweets = data;
   // show each text
-  tweets.map(function (tweet) {
-    console.log(tweet.text);
+  var allTweets = tweets.map(function (tweet) {
+    return tweet.text;
   })
+  var Text = allTweets.join();
+  var inputText = {
+    text: Text
+  }
+
+  tone_analyzer.tone(inputText, getTone);
+
+  function getTone(err, tone) {
+    if (err)
+      console.log(err);
+    else
+      console.log(JSON.stringify(tone, null, 2));
+  }
+
+  personality_insights.profile(inputText, getData);
+
+  function getData(err, response) {
+    if (err)
+      console.log('error', err);
+    else {
+      console.log(JSON.stringify(response, null, 2));
+    }
+  }
 }
